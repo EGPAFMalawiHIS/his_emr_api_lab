a72bbe5b464cf492e6a89ff0f6226b102eca1284
Walter Kaunda <walterkaunda@live.co.uk>
2021-01-08 09:19:18 +0200

    Removed order search by pending results.
    Done in favour of directly pulling individual tests without results.

--- app/services/lab/orders_search_service.rb
+++ app/services/lab/orders_search_service.rb
@@ -6,13 +6,12 @@ module Lab
     class << self
       def find_orders(filters)
         date = filters.delete(:date)
-        pending_results = filters.delete(:pending_results)&.casecmp?('true') || false
 
         orders = Lab::LabOrder.prefetch_relationships
                               .where(filters)
                               .order(start_date: :desc)
+
         orders = filter_orders_by_date(orders, date) if date
-        orders = filter_orders_with_results(orders) if pending_results
 
         orders.map { |order| Lab::LabOrderSerializer.serialize_order(order) }
       end
@@ -22,18 +21,6 @@ module Lab
         end_date = start_date + 1.day
 
         orders.where('start_date >= DATE(?) AND start_date < DATE(?)', start_date, end_date)
-      end
-
-      def filter_orders_with_results(orders)
-        lab_test_concept = ConceptName.where(name: Lab::LabOrder::LAB_TEST_RESULT_CONCEPT_NAME)
-                                      .select(:concept_id)
-        lab_results = Observation.where(concept: lab_test_concept)
-                                 .where.not(obs_group_id: nil)
-                                 .select(:obs_group_id)
-
-        # Select only orders having specimens missing results.
-        orders.left_joins(:specimens)
-              .where.not(obs: { obs_id: lab_results })
       end
     end
   end
