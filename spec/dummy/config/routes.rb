49f1c7ab5098a5278af404bf20b6b10f9ea00adc
Walter Kaunda <walterkaunda@live.co.uk>
2021-01-12 17:26:01 +0200

    Added swagger docs to dummy test app.

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/'
+  mount Rswag::Api::Engine => '/'
   mount Lab::Engine => '/'
-
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
 end
