991dd7b3f6129de99ae692a3a161d2044e708015
Walter Kaunda <walterkaunda@live.co.uk>
2021-02-11 16:33:01 +0200

    Added specimen status to order GET /orders parameters.

--- app/services/lab/orders_search_service.rb
+++ app/services/lab/orders_search_service.rb
@@ -6,12 +6,14 @@ module Lab
     class << self
       def find_orders(filters)
         date = filters.delete(:date)
+        status = filters.delete(:status)
 
         orders = Lab::LabOrder.prefetch_relationships
                               .where(filters)
                               .order(start_date: :desc)
 
         orders = filter_orders_by_date(orders, date) if date
+        orders = filter_orders_by_status(orders, status) if status
 
         orders.map { |order| Lab::LabOrderSerializer.serialize_order(order) }
       end
@@ -21,6 +23,17 @@ module Lab
         end_date = start_date + 1.day
 
         orders.where('start_date >= DATE(?) AND start_date < DATE(?)', start_date, end_date)
+      end
+
+      def filters_orders_by_status(orders, status)
+        case status.downcase
+        when 'ordered' then orders.where(concept_id: unknown_concept_id)
+        when 'drawn' then orders.where.not(concept_id: unknown_concept_id)
+        end
+      end
+
+      def unknown_concept_id
+        ConceptName.find_by_name!('Unknown').concept_id
       end
     end
   end
