3ce76bb798ff89d8c074632dd3951afc89c72371
Walter Kaunda <walterkaunda@live.co.uk>
2021-03-16 17:16:55 +0200

    Appeasing the stupid Rails code autoloader
    For some reason when all tests are run the request test for resource
    test_result_indicators breaks due to failure to autoload the
    ConceptService module. Odd thing is that the test passes when run
    independently.

--- app/controllers/lab/test_result_indicators_controller.rb
+++ app/controllers/lab/test_result_indicators_controller.rb
@@ -4,6 +4,6 @@ class Lab::TestResultIndicatorsController < ApplicationController
   def index
     test_type_id = params.require(:test_type_id)
 
-    render json: ConceptsService.test_result_indicators(test_type_id)
+    render json: Lab::ConceptsService.test_result_indicators(test_type_id)
   end
 end
