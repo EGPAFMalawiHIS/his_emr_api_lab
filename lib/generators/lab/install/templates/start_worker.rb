c5e0ac6f2e5382b8e6245cffc8667435a9642213
Walter Kaunda <walterkaunda@live.co.uk>
2021-03-26 16:11:10 +0200

    Updated worker to only allow one instance per machine.

--- lib/generators/lab/install/templates/start_worker.rb
+++ lib/generators/lab/install/templates/start_worker.rb
@@ -6,11 +6,25 @@ Rails.logger = LoggerMultiplexor.new(Rails.root.join('log/lims-push.log'), $stdo
 api = Lab::Lims::Api.new
 worker = Lab::Lims::Worker.new(api)
 
+def with_lock(lock_file)
+  File.open("log/#{lock_file}", File::RDWR | File::CREAT, 0o644) do |file|
+    unless file.flock(File::LOCK_EX | File::LOCK_NB)
+      Rails.logger.warn("Failed to start new process due to lock: #{lock_file}")
+      exit 2
+    end
+
+    file.rewind
+    file.puts("Process ##{Process.pid} started at #{Time.now}")
+
+    yield
+  end
+end
+
 case ARGV[0]&.downcase
 when 'push'
-  worker.push_orders
+  with_lock('lims-push.lock') { worker.push_orders }
 when 'pull'
-  worker.pull_orders
+  with_lock('lims-pull.lock') { worker.pull_orders }
 else
   warn 'Error: No or invalid action specified: Valid actions are push and pull'
   warn 'USAGE: rails runner start_worker.rb push'
