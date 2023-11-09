e31646719c98bc9ef93bdb45e262d533f09e66ce
Roy Chanunkha <8115806+Skipper-116@users.noreply.github.com>
2023-11-09 15:51:32 +0200

    fixing production errors

--- app/services/lab/lims/exceptions.rb
+++ app/services/lab/lims/exceptions.rb
@@ -2,6 +2,7 @@
 
 module Lab
   module Lims
+    class Exception < StandardError; end
     class LimsException < StandardError; end
     class DuplicateNHID < LimsException; end
     class MissingAccessionNumber < LimsException; end
