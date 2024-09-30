7daa4e3a5d481638a5182d1c1a7622dbbd3dab5c
Andrew Mfune <andiemfune@gmail.com>
2024-09-30 10:25:43 +0200

    test

--- app/controllers/lab/labels_controller.rb
+++ app/controllers/lab/labels_controller.rb
@@ -6,7 +6,7 @@ module Lab
 
     def print_order_label
       order_id = params.require(:order_id)
-
+      raise params[:use_small_specimen_label]
       render json: LabellingService::OrderLabel.new(order_id).print(
         params[:use_small_specimen_label]=="true"
       )
