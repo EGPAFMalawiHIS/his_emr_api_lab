65a773c60ee3bc5cdc33be60aa4b95df60abd75f
Hopson Gausi <hopgausi20@gmail.com>
2024-10-09 11:26:53 +0200

    added param for choosing small label or not

--- app/controllers/lab/labels_controller.rb
+++ app/controllers/lab/labels_controller.rb
@@ -6,8 +6,7 @@ module Lab
 
     def print_order_label
       order_id = params.require(:order_id)
-
-      render_zpl(LabellingService::OrderLabel.new(order_id).print)
+      render_zpl(LabellingService::OrderLabel.new(order_id).print(params[:use_small_specimen_label] == 'true'))
     end
   end
 end
