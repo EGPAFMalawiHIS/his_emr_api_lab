382dcfa0b40732245519ed59de67e2ff7bb1e699
Andrew Mfune <andiemfune@gmail.com>
2025-01-22 12:00:31 +0200

    Support for parameter specified print copies
    Added support for print copies in the controller params. This provides extreme flexibility for apps to specify copies without having to use global properties

--- app/controllers/lab/labels_controller.rb
+++ app/controllers/lab/labels_controller.rb
@@ -6,7 +6,12 @@ module Lab
 
     def print_order_label
       order_id = params.require(:order_id)
-      render_zpl(LabellingService::OrderLabel.new(order_id).print(params[:use_small_specimen_label] == 'true'))
+      print_copies = params[:number_of_copies].to_i if params[:number_of_copies].present?
+      render_zpl(LabellingService::OrderLabel.new(order_id).print(
+          params[:use_small_specimen_label] == 'true', 
+          print_copies        
+        )
+      )
     end
   end
 end
