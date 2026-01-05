9f89a464f26f351b687e566b0613e9b7c9482a69
Hopson Gausi <hopgausi20@gmail.com>
2026-01-05 13:51:21 +0200

    refactor: update authentication handling and modify Rails dependency version

--- app/controllers/lab/orders_controller.rb
+++ app/controllers/lab/orders_controller.rb
@@ -2,7 +2,6 @@
 
 module Lab
   class OrdersController < ApplicationController
-    skip_before_action :authenticate, only: %i[order_status order_result summary]
     before_action :authenticate_request, only: %i[order_status order_result summary]
 
     def create
