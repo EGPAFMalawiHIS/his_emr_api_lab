341cfaf1f0752a34d546f9ef24e84ad88a433661
brian@xps <brianmsyamboza@gmail.com>
2026-01-22 13:02:25 +0200

    fix pulling of data from test catalog

--- app/services/lab/lims/exceptions.rb
+++ app/services/lab/lims/exceptions.rb
@@ -11,3 +11,14 @@ module Lab
     end
   end
 end
+
+
+module Lab
+  module Lims
+      class LimsException < StandardError; end
+      class DuplicateNHID < LimsException; end
+      class MissingAccessionNumber < LimsException; end
+      class UnknownSpecimenType < LimsException; end
+      class UnknownTestType < LimsException; end
+  end
+end
