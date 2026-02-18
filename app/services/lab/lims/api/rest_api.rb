4f61cf42331bad345963b97c32eed63d222a83fb
Hopson Gausi <hopgausi20@gmail.com>
2026-02-18 11:12:10 +0200

    feat: update test result handling to include status trail and use PUT for updates

--- app/services/lab/lims/api/rest_api.rb
+++ app/services/lab/lims/api/rest_api.rb
@@ -437,23 +437,25 @@ module Lab
           order_dto['test_results'].each do |test_name, results|
             Rails.logger.info("Pushing result for order ##{order_dto['tracking_number']}")
             in_authenticated_session do |headers|
-              params = make_update_test_params(order_dto['tracking_number'], test_name, results)
+              params = make_update_test_params(order_dto, test_name, results)
 
-              RestClient.post(expand_uri("tests/#{order_dto['tracking_number']}", api_version: 'v2'), params, headers)
+              RestClient.put(expand_uri("tests/#{order_dto['tracking_number']}", api_version: 'v2'), params, headers)
             end
           end
         end
 
-        def make_update_test_params(_tracking_number, test, results, test_status = 'Drawn')
+        def make_update_test_params(order_dto, test, results, test_status = 'Drawn')
+          # Find the concept from the test name (which is a string)
+          concept = ::ConceptName.find_by(name: test)&.concept
           {
             test_status:,
             time_updated: results['result_date'],
             test_type: {
-              name: ::Concept.find(test.concept_id).test_catalogue_name,
-              nlims_code: ::Concept.find(test.concept_id).nlims_code
+              name: concept&.test_catalogue_name,
+              nlims_code: concept&.nlims_code
             },
-            test_results: results['results'].map do |measure, _value|
-              measure_name, measure_value = measure
+            test_results: results['results'].map do |measure_name, value|
+              measure_value = value['result_value']
               {
                 measure: {
                   name: measure_name,
@@ -463,6 +465,18 @@ module Lab
                 result: {
                   value: measure_value,
                   result_date: results['result_date']
+                }
+              }
+            end,
+            status_trail: order_dto['sample_statuses'].map do |trail_entry|
+              date, status = trail_entry.each_pair.first
+              {
+                status: status['status'],
+                timestamp: date,
+                updated_by: {
+                  first_name: status.fetch('updated_by').fetch('first_name'),
+                  last_name: status.fetch('updated_by').fetch('last_name'),
+                  id_number: status.fetch('updated_by').fetch('id')
                 }
               }
             end
