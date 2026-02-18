09ce89a582cffb12bb7cc9f66f00146b48ba650f
Hopson Gausi <hopgausi20@gmail.com>
2026-02-18 10:21:11 +0200

    fix: improve location context handling in ProcessLabResultJob and update 'Ordered By' field in ResultsService

--- app/jobs/lab/process_lab_result_job.rb
+++ app/jobs/lab/process_lab_result_job.rb
@@ -7,7 +7,10 @@ module Lab
     queue_as :default
     def perform(results_obs_id, serializer, result_enter_by)
       Rails.logger.info("Lab::ProcessLabResultJob: Processing result completion for #{serializer}")
+      # set location context for the job based on the order's encounter to ensure proper context for any operations performed in the job
       results_obs = Lab::LabResult.find(results_obs_id)
+      encounter = Encounter.unscoped.find_by(encounter_id: results_obs.encounter_id)
+      Location.current = Location.find(encounter.location_id) if encounter&.location_id
       Lab::ResultsService.process_result_completion(results_obs, serializer, result_enter_by)
     end
   end
