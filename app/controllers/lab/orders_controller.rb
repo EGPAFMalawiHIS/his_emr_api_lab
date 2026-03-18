77936c2827f747db8c461f6d2e04d1d2a64a80ea
Hopson Gausi <hopgausi20@gmail.com>
2026-03-18 15:06:09 +0200

    Add visit_id to order response and update authentication skip behavior

--- app/controllers/lab/orders_controller.rb
+++ app/controllers/lab/orders_controller.rb
@@ -2,7 +2,7 @@
 
 module Lab
   class OrdersController < ApplicationController
-    skip_before_action :authenticate, only: %i[order_status order_result summary]
+    skip_before_action :authenticate, only: %i[order_status order_result summary], raise: false
     before_action :authenticate_request, only: %i[order_status order_result summary]
 
     def create
